2f830b4e29ba7a220ba67322eff940467eb8ed6e
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-20 18:06:39 +0200

    Added States

--- components/TopTab2.js
+++ components/TopTab2.js
@@ -1,36 +1,117 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Text, View, FlatList, TextInput, StyleSheet, Button } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 function HomeScreen() {
+  const [Items, setItems] = React.useState([
+    { key: "1", name: "Item1" },
+    { key: "2", name: "Item1" },
+    { key: "3", name: "Item1" },
+    { key: "4", name: "Item1" },
+  ]);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
+    <View style={{ flex: 1}}>
+      <FlatList
+        data={Items}
+        renderItem={({ item }) => (
+          <Text style={{ margin: 10 }}>{item.name}</Text>
+        )}
+      />
     </View>
   );
 }
 
 function SettingsScreen() {
+    const [name , setName] = React.useState('');
+    const [number, setNumber] = React.useState(5);
+
+    const submit = () => {
+     setNumber(number + 5);
+    }  
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
+     <TextInput 
+     style={styles.input}
+     placeholder="Enter your name"
+     value={name}
+     onChangeText={text => setName(text)}
+     />
+     <Text style={{marginTop: 10, fontSize: 17}}>Your name is {name}</Text>
+     <Text style={{marginTop: 10, fontSize: 17}}>Number is {number}</Text>
+     <View style={{height: 40, width: 250, marginTop: 10,}}> 
+
+     <Button title="Submit Form" onPress={submit}></Button>
+     </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+    input:
+    {
+        borderBottomWidth: 1,
+        height: 40,
+        width: 300,
+    }
+})
 function ProfileScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Profile!</Text>
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "space-around",
+        alignItems: "center",
+        flexDirection: "row",
+      }}
+    >
+      <View
+        style={{
+          height: 100,
+          width: 100,
+          backgroundColor: "blue",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text>1</Text>
       </View>
-    );
-  }
+      <View
+        style={{
+          height: 100,
+          width: 100,
+          backgroundColor: "blue",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          height: 100,
+          width: 100,
+          backgroundColor: "blue",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text>3</Text>
+      </View>
+    </View>
+  );
+}
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function TopBar() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        options={{
+          title: "Home",
+        }}
+        component={HomeScreen}
+      />
       <Tab.Screen name="Settings" component={SettingsScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
