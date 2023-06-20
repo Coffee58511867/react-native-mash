9ce8ed0d479c38b0c4083e661de875b41fe875b6
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-20 16:01:46 +0200

    Styled screen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text, Button , StyleSheet, TouchableOpacity} from "react-native";
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Register from "./components/Register";
@@ -14,7 +14,10 @@ function HomeScreen({ navigation }) {
     <View style={styles.container}>
       <Text>Home Screen</Text>
       <TouchableOpacity style={styles.btn}>
-        <Text style={styles.btnTex} onPress={() => navigation.push("Create an Account")}>
+        <Text
+          style={styles.btnTex}
+          onPress={() => navigation.push("Create an Account")}
+        >
           Create an Account
         </Text>
       </TouchableOpacity>
@@ -24,17 +27,26 @@ function HomeScreen({ navigation }) {
         </Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.btn}>
-        <Text style={styles.btnTex} onPress={() => navigation.push("Dashboard")}>
+        <Text
+          style={styles.btnTex}
+          onPress={() => navigation.push("Dashboard")}
+        >
           Bottom Tabs
         </Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.btn}>
-        <Text style={styles.btnTex} onPress={() => navigation.push("Admin Dashboard")}>
+        <Text
+          style={styles.btnTex}
+          onPress={() => navigation.push("Admin Dashboard")}
+        >
           Top Tabs
         </Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.btn}>
-        <Text style={styles.btnTex} onPress={() => navigation.push("My Dashboard")}>
+        <Text
+          style={styles.btnTex}
+          onPress={() => navigation.push("My Dashboard")}
+        >
           Top Tab 2
         </Text>
       </TouchableOpacity>
@@ -44,13 +56,13 @@ function HomeScreen({ navigation }) {
 
 function DetailsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen</Text>
       <Button
         title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
+        onPress={() => navigation.push("Details")}
       />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
         title="Go back to first screen in stack"
@@ -65,17 +77,54 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home" 
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: '#f4511e',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+              headerTitleAlign: "center", 
+            }}
+      >
+        <Stack.Screen
+          name="Home"
+          options={{
+            title: "My home",
+            headerStyle: {
+              backgroundColor: "#f4511e",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerTitleAlign: "center", 
+  
+          }}
+          component={HomeScreen}
+        />
         <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="Create an Account" 
-        
-        component={Register} />
+        <Stack.Screen name="Create an Account" component={Register} />
         <Stack.Screen name="Details Page" component={DetailsPage} />
         <Stack.Screen name="Tabs" component={Tabs} />
         <Stack.Screen name="Dashboard" component={BottomNav} />
         <Stack.Screen name="Admin Dashboard" component={TabViewExample} />
-        <Stack.Screen name="My Dashboard" component={TopBar} />
+        <Stack.Screen
+          name="My Dashboard"
+          options={{
+            title: "My Dashboard",
+            headerStyle: {
+              backgroundColor: "#f4511e",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+          component={TopBar}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -105,7 +154,6 @@ const styles = StyleSheet.create({
     marginTop: 9,
     fontSize: 16,
   },
-
 });
 
 export default App;
